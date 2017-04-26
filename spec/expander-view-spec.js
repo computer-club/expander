1a4edbbbdf54fa56fef90c8ddf1858505f3a75a3
Salvatore Tosti <saltosti@gmail.com>
2017-04-25 17:30:49 -0700

    Initial commit

---
+++ spec/expander-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ExpanderView from '../lib/expander-view';
+
+describe('ExpanderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
